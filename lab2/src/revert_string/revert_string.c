6f2cd6d9df3503e25bf1f1f526870a33390a52fb
oliabka <oliabka@gmail.com>
2021-02-14 10:01:10 +0000

    added files for lab2 task2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
-#include "revert_string.h"
+#include <string.h>
 
+#include "revert_string.h"
+char *strrev(char *str)
+{
+      char *p1, *p2;
+
+      if (! str || ! *str)
+            return str;
+      for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+      {
+            *p1 ^= *p2;
+            *p2 ^= *p1;
+            *p1 ^= *p2;
+      }
+      return str;
+}
 void RevertString(char *str)
 {
-	// your code here
+    strrev(str);
 }
-
