ca14500ec02a90abc2ca414aaade398740bab964
oliabka <oliabka@gmail.com>
2021-03-06 19:39:48 +0000

    commentary added to parallel script

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -31,15 +31,16 @@ int main(int argc, char **argv) {
                                       {0, 0, 0, 0}};
 
     int option_index = 0;
-    int c = getopt_long(argc, argv, "f", options, &option_index);
+    int c = getopt_long(argc, argv, "f", options, &option_index);  // https://habr.com/ru/post/55665/
 
-    if (c == -1) break;
+    if (c == -1) break;   //короче эта штука считывает введеннные пользователем параметры и в зависимости от введенных символов
+                          // определяет как работать с функцией 
 
     switch (c) {
-      case 0:
-        switch (option_index) {
+      case 0:         // если getopt_long() вернул 0, то работаем с fork() (я так поняла, это если мы не ввели f и не вызвали ошибку)
+        switch (option_index) {    //проходимся по списку options и для первых трех считываем значения обязательных аргументов
           case 0:
-            seed = atoi(optarg);
+            seed = atoi(optarg);     // atoi() используется для приведения (конвертации) строки в числовой вид
             // your code here
             // error handling
             break;
@@ -61,47 +62,49 @@ int main(int argc, char **argv) {
             printf("Index %d is out of options\n", option_index);
         }
         break;
-      case 'f':
+      case 'f':          //если ввели f, работаем с файлами
         with_files = true;
         break;
 
-      case '?':
-        break;
+      case '?':          //если ошибка, выводим код считанного ошибочного символа
+        break; 
 
       default:
         printf("getopt returned character code 0%o?\n", c);
     }
   }
-
+  // проверка полученных данных для создания массива
   if (optind < argc) {
     printf("Has at least one no option argument\n");
     return 1;
   }
-
   if (seed == -1 || array_size == -1 || pnum == -1) {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
     return 1;
   }
 
-  int *array = malloc(sizeof(int) * array_size);
+  //формирование массива
+  int *array = malloc(sizeof(int) * array_size); 
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
 
+  //переменная времени
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
+ //подключение многопоточности
   for (int i = 0; i < pnum; i++) {
-    pid_t child_pid = fork();
+    pid_t child_pid = fork();      // https://www.youtube.com/watch?v=9seb8hddeK4
     if (child_pid >= 0) {
-      // successful fork
+      // successful fork 
       active_child_processes += 1;
       if (child_pid == 0) {
         // child process
 
         // parallel somehow
 
-        if (with_files) {
+        if (with_files) {          //если работаем с файлами
           // use files here
         } else {
           // use pipe here
@@ -115,8 +118,8 @@ int main(int argc, char **argv) {
     }
   }
 
-  while (active_child_processes > 0) {
-    // your code here
+  while (active_child_processes > 0) {            // пока есть активные процессы-дети, для каждого что-то выполдняем  и он 
+    // your code here                             //заканчивает работу, после чего учитываем это в числе активных детей
 
     active_child_processes -= 1;
   }
@@ -139,12 +142,14 @@ int main(int argc, char **argv) {
     if (max > min_max.max) min_max.max = max;
   }
 
+   //структура времени завершения
   struct timeval finish_time;
   gettimeofday(&finish_time, NULL);
-
+  // подсчет прошедшего времени
   double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
+  //очистка массива и вывод данных
   free(array);
 
   printf("Min: %d\n", min_max.min);
