befbe62e15896c1dc9e0a9e00034f3bbcb3d0c3a
oliabka <oliabka@gmail.com>
2021-03-06 19:00:33 +0000

    files added for lab3 part1

--- lab3/src/sequential_min_max.c
+++ lab3/src/sequential_min_max.c
@@ -5,18 +5,18 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
+  if (argc != 3) {     //checking if 2 arguments were given 
     printf("Usage: %s seed arraysize\n", argv[0]);
     return 1;
   }
 
-  int seed = atoi(argv[1]);
+  int seed = atoi(argv[1]); //first argument given
   if (seed <= 0) {
     printf("seed is a positive number\n");
     return 1;
   }
 
-  int array_size = atoi(argv[2]);
+  int array_size = atoi(argv[2]);  //second argument given
   if (array_size <= 0) {
     printf("array_size is a positive number\n");
     return 1;
