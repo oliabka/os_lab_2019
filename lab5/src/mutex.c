dd68e92826906e088e34595c95721ade3db4dba7
oliabka <oliabka@gmail.com>
2021-03-25 17:20:11 +0000

    some commentary

--- lab5/src/mutex.c
+++ lab5/src/mutex.c
@@ -10,6 +10,8 @@
  *
  * Simple multi-threaded example with a mutex lock.
  */
+
+ /*  Для компиляции -lpthread */
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -25,6 +27,7 @@ pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 int main() {
   pthread_t thread1, thread2;
 
+/* Create independent threads each of which will execute function */
   if (pthread_create(&thread1, NULL, (void *)do_one_thing,
 			  (void *)&common) != 0) {
     perror("pthread_create");
@@ -36,6 +39,10 @@ int main() {
     perror("pthread_create");
     exit(1);
   }
+
+ /* Wait till threads are complete before main continues. Unless we  */
+     /* wait we run the risk of executing an exit which will terminate   */
+     /* the process and all threads before the threads have completed.   */
 
   if (pthread_join(thread1, NULL) != 0) {
     perror("pthread_join");
@@ -51,6 +58,14 @@ int main() {
 
   return 0;
 }
+
+/* Mutexes are used to prevent data inconsistencies due to race conditions. 
+ A race condition often occurs when two or more threads need to perform operations on the same memory area,
+ but the results of computations depends on the order in which these operations are performed. 
+ Mutexes are used for serializing shared resources. 
+ Anytime a global resource is accessed by more than one thread the resource should have a Mutex associated with it. 
+ One can apply a mutex to protect a segment of memory ("critical region") from other threads. 
+ Mutexes can be applied only to threads in a single process and do not work between processes as do semaphores.*/
 
 void do_one_thing(int *pnum_times) {
   int i, j, x;
